2d038ffaf804fc0dd67d1575567313c13165a067
Joni Virtanen <vijoni91@gmail.com>
2018-11-20 20:59:23 +0200

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sum = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.reduce(sum, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
