82c240b207c5d9640cb783adf99971f82d62a6b1
Joni Virtanen <vijoni91@gmail.com>
2018-11-20 18:46:44 +0200

    4.2 - 1

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const http = require('http')
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
-const morgan = require('morgan')
+const middleware = require('./utils/middleware')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const mongoUrl = process.env.MONGODB_URI
+
+mongoose.connect(mongoUrl, {useNewUrlParser: true})
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+app.use(middleware.logger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.error)
 
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
